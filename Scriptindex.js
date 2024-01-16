6bbce0808af024173b0b47de842a64d6ebd13afc
SalehC77 <saleh774361224@gmail.com>
2024-01-16 18:38:18 +0300

    change2

--- Scriptindex.js
+++ Scriptindex.js
@@ -3,8 +3,40 @@
 let Content = document.getElementById("Change_Content")
 let Key_Right = document.getElementById("Right")
 let Key_Lift = document.getElementById("Lift")
+ 
 
-Key_Right.onclick = function () {
+/*Key_Right.addEventListener()*/
 
-    Content.innerHTML =
-}
+    function chang_Right(i) {
+       
+            if (i == 0) {
+                Content.te = "»„Ã—œ „« √‰ Ì÷Ì¡ «·⁄ﬁ· ›·« Ì„ﬂ‰ √‰ Ì⁄Êœ „Ÿ·„« „—… √Œ—Ï"
+
+                i++
+            }
+            if (i == 1) {
+                Content.textContent = "·‰   ﬁœ„ «·√„„ ≈·« »«· ⁄·Ì„° ›«· ⁄·Ì„ ÂÊ „ﬁÌ«” „Â„ Ãœ« ·ﬁÌ«” ‰Â÷… «·‘⁄Ê»"
+                i++
+            }
+            
+       
+       
+
+    }
+    function chang_Lift(i) {
+
+        if (i == 1) {
+            Content.textContent = "saleh"
+
+            i--;
+        }
+        if (i == 2) {
+            Content.textContent = "ahmed"
+            i--;
+        }
+
+
+    }
+
+
+
