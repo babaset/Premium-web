3d991d85b0a2cf34b6ea2f6ecebf37ba183df32d
SalehC77 <saleh774361224@gmail.com>
2024-01-16 17:10:47 +0300

    chnge_main

---
+++ Scriptindex.js
@@ -0,0 +1,10 @@
+// JavaScript source code
+
+let Content = document.getElementById("Change_Content")
+let Key_Right = document.getElementById("Right")
+let Key_Lift = document.getElementById("Lift")
+
+Key_Right.onclick = function () {
+
+    Content.innerHTML =
+}
